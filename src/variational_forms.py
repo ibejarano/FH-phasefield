4ec8e1fef5288b4433705273f1ee632cf18caa11
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2025-06-02 17:31:55 -0300

    variational forms to fenicsx

--- src/variational_forms.py
+++ src/variational_forms.py
@@ -1,4 +1,5 @@
-from dolfin import inner, grad, dx, dot, Measure, Constant
+import ufl
+from dolfinx.fem import Constant
 
 def define_variational_forms(epsilon, sigma_func, H, phase, displacement, data, boundary_markers, E_expr, nu):
     Gc = data["Gc"]
@@ -6,10 +7,9 @@ def define_variational_forms(epsilon, sigma_func, H, phase, displacement, data, 
     p_init = data.get("p_init", 100.0)
     pxx = data["px"]
 
-    pressure = Constant(p_init)
-    ds = Measure("ds", subdomain_data=boundary_markers)
-    px_vec = Constant((pxx, 0.0))
-
+    pressure = Constant(displacement.function_space.mesh, p_init)
+    ds = ufl.Measure("ds", domain=displacement.function_space.mesh, subdomain_data=boundary_markers)
+    px_vec = Constant(displacement.function_space.mesh, (pxx, 0.0))
 
     p = phase.get_trialfunction()
     q = phase.get_testfunction()
@@ -19,12 +19,12 @@ def define_variational_forms(epsilon, sigma_func, H, phase, displacement, data, 
 
     sigma = sigma_func(u, E_expr, nu)
 
-    E_du = (1 - pold)**2 * inner(epsilon(v), sigma) * dx \
-           + pressure * inner(v, grad(pold)) * dx \
-           + dot(px_vec, v) * ds(10) - dot(px_vec, v) * ds(30)
+    E_du = (1 - pold)**2 * ufl.inner(epsilon(v), sigma) * ufl.dx \
+           + pressure * ufl.inner(v, ufl.grad(pold)) * ufl.dx \
+           + ufl.dot(px_vec, v) * ds(10) - ufl.dot(px_vec, v) * ds(30)
 
-    E_phi = (Gc * l * inner(grad(p), grad(q)) \
-             + ((Gc / l) + 2.0 * H) * inner(p, q) \
-             - 2.0 * H * q) * dx
+    E_phi = (Gc * l * ufl.inner(ufl.grad(p), ufl.grad(q))
+             + ((Gc / l) + 2.0 * H) * ufl.inner(p, q)
+             - 2.0 * H * q) * ufl.dx
 
     return E_du, E_phi, pressure
