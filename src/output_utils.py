ddcf2a2ca8628babd895f62b662bdbf123c8cd69
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2025-06-02 17:39:05 -0300

    outputs to fenicsx

--- src/output_utils.py
+++ src/output_utils.py
@@ -1,20 +1,21 @@
-from dolfin import File, TimeSeries, XDMFFile
+#from dolfin import File, TimeSeries, XDMFFile
+from dolfin.io import XDMFFile, TimeSeries
 
-def create_output_files(caseDir):
-    xdmf = XDMFFile(f"{caseDir}/output.xdmf")
-    xdmf.parameters["flush_output"] = True
-    xdmf.parameters["functions_share_mesh"] = True
-
-    u_ts = TimeSeries(f"{caseDir}/u_series")
-    phi_ts = TimeSeries(f"{caseDir}/phi_series")
-
-    return xdmf, u_ts, phi_ts
+def create_output_files(case_dir, comm):
+    xdmf = XDMFFile(comm, f"{case_dir}/output.xdmf", "w")
+    xdmf.write_mesh = True
+    return xdmf
 
 def write_output(xdmf, u, phi, stress, t):
-    xdmf.write(u, t)
-    xdmf.write(phi, t)
-    xdmf.write(stress, t)
+    xdmf.write_function(u, t)
+    xdmf.write_function(phi, t)
+    xdmf.write_function(stress, t)
 
-def store_time_series(u_ts, phi_ts, u, phi, t):
-    u_ts.store(u.vector(), t)
-    phi_ts.store(phi.vector(), t)
+def store_snapshot(u, phi, t, caseDir):
+    # Guarda los campos como archivos XDMF individuales (opcional)
+    with XDMFFile(u.function_space.mesh.comm, f"{caseDir}/u_{t:.4f}.xdmf", "w") as xdmf_u:
+        xdmf_u.write_mesh(u.function_space.mesh)
+        xdmf_u.write_function(u, t)
+    with XDMFFile(phi.function_space.mesh.comm, f"{caseDir}/phi_{t:.4f}.xdmf", "w") as xdmf_phi:
+        xdmf_phi.write_mesh(phi.function_space.mesh)
+        xdmf_phi.write_function(phi, t)
