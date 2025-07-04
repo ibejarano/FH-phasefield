bd7a400f2531878ca8202b9b2c2149503ae7dc64
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2025-07-04 15:21:38 -0300

    job slurm and sync bash

--- run_job.sh
+++ run_job.sh
@@ -11,8 +11,6 @@ eval "$(conda shell.bash hook)"
 conda activate fenicsproject
 export OMP_NUM_THREADS=1
 NAME=slurm_job_${SLURM_JOB_ID}
-H=10
 
-# Comando a ejecutar — reemplazá esto por lo que quieras
-python3 meshing.py gmsh_1 name=$NAME l_max=1.5 h=1e-3 H=$H
-python3 main.py gmsh_1 name=$NAME H=$H t_max=0.2 l_max=1.5
+python3 meshing.py --config data/gmsh_1.json --case_dir results/$NAME
+python3 main.py --config data/gmsh_1.json --case_dir $NAME
