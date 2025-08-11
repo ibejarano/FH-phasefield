4be5773a5a688eb9466c5211570644f4b4e1f106
Ignacio Bejarano <ignbejarano@gmail.com>
2025-08-11 09:17:17 -0300

    fix normal traction

--- examples/lefm/angled_notch/caso_3.py
+++ examples/lefm/angled_notch/caso_3.py
@@ -15,8 +15,8 @@ V = VectorFunctionSpace(mesh, "P", 1)
 tol = 1E-5
 
 
-bc_bottom = DirichletBC(V.sub(0), Constant(0), boundaries, 2)
-bc_top = DirichletBC(V.sub(0), Constant(0), boundaries, 4)
+bc_bottom = DirichletBC(V, Constant((0,0 )), boundaries, 2)
+bc_top = DirichletBC(V, Constant((0, 0)), boundaries, 4)
 
 bcs = [bc_bottom, bc_top]
 E = 164.3e9
@@ -27,7 +27,7 @@ mu = E / (2 * (1 + nu))
 lmbda = E*nu / ((1 + nu)*(1 - 2*nu))
 # lmbda = 2 * mu * lmbda / (lmbda + 2 * mu)
 
-ds = ds(subdomain_data=boundaries)
+ds = Measure("ds", domain=mesh, subdomain_data=boundaries)
 u = TrialFunction(V)
 v = TestFunction(V)
 
@@ -35,8 +35,9 @@ a = elastic_energy_funcional(u, v, lmbda, mu)
 
 p1 = 183e6*0.02
 
-upper_traction = Constant((0.0, p1))
-L_form  = dot(upper_traction, v)*ds(4) - dot(upper_traction, v)*ds(2)
+n = FacetNormal(mesh)
+
+L_form  = - dot(p1*n, v)*ds(10) - dot(p1*n, v)*ds(11)
 u_sol = Function(V, name="desplazamientos")
 solve(a == L_form, u_sol, bcs)
 
@@ -47,11 +48,9 @@ kappa = 3 - 4 *nu # Plane - strain
 factor = np.sqrt(2 * np.pi) * mu / (1+kappa)
 rel_KI = (p1 * np.sqrt(np.pi * Lcrack))
 
-
 npoints = 500
 
 beta_rad = np.deg2rad(beta)
-print("Beta", beta, beta_rad)
 xs = np.linspace(Lcrack*0.5*np.cos(beta_rad), Lcrack*np.cos(beta_rad), npoints)
 ys = np.linspace(Lcrack*0.5*np.sin(beta_rad), Lcrack*np.sin(beta_rad), npoints)
 
@@ -86,9 +85,6 @@ dV = np.abs(uplus_res[:, 0] - uminus_res[:, 0])
 
 KI_teo = p1 * np.sqrt(np.pi * Lcrack) * (np.cos(beta_rad)**2 + alpha * np.sin(beta_rad)**2)
 KII_teo = p1 * np.sqrt(np.pi * Lcrack) * (1-alpha) * np.sin(beta_rad) * np.cos(beta_rad)
-print("Teo KI" , KI_teo / rel_KI)
-print("-------")
-print("Teo KII" , KII_teo / rel_KI)
 
 file = File('caso3.pvd')
 file << u_sol
